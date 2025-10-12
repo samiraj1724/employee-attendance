a6e8609e0a5ae5628b1c6e6a730df31c42674e0f
samiraj1724 <samikhyapanigrahi.43@gmail.com>
2025-10-12 20:39:32 +0530

    Update EmployeeApp.java

--- src/main/java/com/attendance/EmployeeApp.java
+++ src/main/java/com/attendance/EmployeeApp.java
@@ -2,24 +2,10 @@ package com.attendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class EmployeeApp {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApp.class, args);
-    }
-
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to Employee Attendance System!";
-    }
-
-    @GetMapping("/mark")
-    public String markAttendance() {
-        return "Attendance marked successfully!";
     }
 }
